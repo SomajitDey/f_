eb3f79e840e1cf03e97e35e827329f1125c83bb4
Somajit Dey <somajit@users.sourceforge.net>
2021-01-26 19:47:22 +0530

    LGPL-2.1-or-later

--- syscall_wrapper.f90
+++ syscall_wrapper.f90
@@ -1,22 +1,22 @@
-!@Somajit Dey <somajit@users.sourceforge.net> 4 January 2021
+!@Somajit Dey <somajit@users.sourceforge.net> January 2021
 !
-! Copyright (C) 2020 Somajit Dey
+! Copyright (C) 2020-2021 Somajit Dey
 ! Department of Physics, University of Calcutta
 ! Email: somajit@users.sourceforge.net
 ! 
-! This file is part of progrep.
+! This file is part of f_
 ! 
-! progrep is free software: you can redistribute it and/or modify it
-! under the terms of the GNU General Public License as published by the
-! Free Software Foundation, either version 3 of the License, or (at your
+! f_ is a free software library: you can redistribute it and/or modify it
+! under the terms of the GNU Lesser General Public License as published by the
+! Free Software Foundation, either version 2.1 of the License, or (at your
 ! option) any later version.
 !   
-! progrep is distributed in the hope that it will be useful, but
+! f_ is distributed in the hope that it will be useful, but
 ! WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-! General Public License for more details.
+! Lesser General Public License for more details.
 !   
-! You should have received a copy of the GNU General Public License
+! You should have received a copy of the GNU Lesser General Public License
 ! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 !************************************************************************
 
