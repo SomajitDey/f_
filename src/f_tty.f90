af22d5acc84f345fab308d58d1519e08ad86a999
Somajit Dey <somajit@users.sourceforge.net>
2021-02-01 20:18:28 +0530

    sanity break

--- src/f_tty.f90
+++ src/f_tty.f90
@@ -41,19 +41,42 @@
 ! not support tput will not be compatible.
 !************************************************************************
 
+! REFERENCE:
+! Command: read --help
+! For tput: http://linuxcommand.org/lc3_adv_tput.php
+!************************************************************************
+
 module f_tty
 
 implicit none
 private
 
+integer, parameter :: red=1, green=2, yellow=3, blue=4, magenta=5, cyan=6
+
 !~~~~~~~~~~~~~~~~~~~~~~~~~BEGIN CONTENTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-public :: f_getch, f_bold, f_outstanding, f_clrscr
+!Input single character without pressing Enter
+public :: f_getch
+
+!Display style
+public :: f_bold, f_outstanding, f_underline, f_italic, f_blink
+
+!Display color
+public :: f_red, f_green, f_yellow, f_blue, f_magenta, f_cyan
+
+!Get #Lines(Rows) and #Columns in terminal window
+public :: f_termheight, f_termwidth
+
+!Clear screen
+public :: f_clrscr
+
+!Prompt or Cursor
+public :: f_hideprompt, f_showprompt
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~END CONTENTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 contains
 
 character function f_getch(echo) !Works for bash only
-logical, optional :: echo
+logical, intent(in), optional :: echo
 if(present(echo))then
     if(echo)call execute_command_line('read -n 1 f_getch_private')
 else
@@ -62,29 +85,52 @@ endif
 call get_environment_variable('f_getch_private',f_getch)
 end function f_getch
 
-subroutine f_bold(string)
-character(len=*) :: string
+subroutine f_bold(string,noadvance)
+character(len=*), intent(in) :: string
 call execute_command_line('tput bold && echo '//string//' && tput sgr0')
 end subroutine f_bold
 
 subroutine f_outstanding(string)
-character(len=*) :: string
+character(len=*), intent(in) :: string
 call execute_command_line('tput smso && echo '//string//' && tput rmso')
 end subroutine f_outstanding
 
-subroutine f_clrscr(nrows)
-integer, optional :: nrows
-character(len=4) :: nlines
-if(present(nrows))then
-    write(nlines,'(I0)')nrows
-    if(nrows<0)then
-        call execute_command_line('tput clear')
+subroutine f_underline(string)
+character(len=*), intent(in) :: string
+call execute_command_line('tput smul && echo '//string//' && tput rmul')
+end subroutine f_underline
+
+subroutine f_italic(string)
+character(len=*), intent(in) :: string
+call execute_command_line('tput sitm && echo '//string//' && tput ritm')
+end subroutine f_italic
+
+subroutine f_blink(string)
+character(len=*), intent(in) :: string
+call execute_command_line('tput blink && echo '//string//' && tput sgr0')
+end subroutine f_blink
+
+subroutine f_clrscr(nlines)
+integer, intent(in), optional :: nlines
+character(len=4) :: nrows
+if(present(nlines))then
+    write(nrows,'(I0)')nlines
+    if(nlines<0)then
+        call execute_command_line('tput reset')
     else
-        call execute_command_line('tput cuu '//trim(nlines)//' && tput ed')
+        call execute_command_line('tput cuu '//trim(nrows)//' && tput ed')
     endif
 else
-    call execute_command_line('tput reset')
+    call execute_command_line('tput clear')
 endif
 end subroutine f_clrscr
+
+subroutine f_hideprompt()
+call execute_command_line('tput civis')
+end subroutine f_hideprompt
+
+subroutine f_showprompt()
+call execute_command_line('tput cnorm')
+end subroutine f_showprompt
 
 end module f_tty
